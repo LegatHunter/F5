e8d0c1987685dd5c214e4b4ed3dd5aed3c151d70
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-10-07 11:05:47 +0300

    F5

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 const swiper = new Swiper('.work__slider', {
-  slidesPerView: 3, // По умолчанию показывать 3 карточки
-  spaceBetween: 30, // Расстояние между карточками
+  slidesPerView: 3,
+  spaceBetween: 30,
   loop: true,
   autoplay: {
-    delay: 2500, // 2.5 секунды
+    delay: 2500,
     disableOnInteraction: false,
   },
   navigation: {
     nextEl: '.work__btn_right',
     prevEl: '.work__btn_left',
   },
-  // Настройка для разных экранов
   breakpoints: {
     375: {
-      slidesPerView: 1, // Одна карточка на экранах от 375px до 500px
-      spaceBetween: 10, // Меньшее расстояние между карточками на мобильных
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
     500: {
-      slidesPerView: 2, // 2 карточки для экранов шириной от 500px
+      slidesPerView: 2,
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 3, // Для больших экранов по умолчанию 3 карточки
+      slidesPerView: 3,
       spaceBetween: 30,
     }
   }
 });
 
-// Остановка автоплей на ховере
+
 const sliderElement = document.querySelector('.work__slider');
 
 sliderElement.addEventListener('mouseenter', () => {
