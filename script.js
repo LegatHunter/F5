f6a2df504a381cccbbf473b1385e1b9d7efba5e6
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-10-07 18:10:57 +0300

    F5 2 slider done!

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-const swiper = new Swiper('.work__slider', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.work__btn_right',
-    prevEl: '.work__btn_left',
-  },
-  breakpoints: {
-    375: {
-      slidesPerView: 1,
-      spaceBetween: 10,
+function workSlider(newDelay = 2500) {
+  const swiper = new Swiper(".work__slider", {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: {
+      delay: newDelay,
+      disableOnInteraction: false,
     },
-    500: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+    navigation: {
+      nextEl: ".work__btn_right",
+      prevEl: ".work__btn_left",
     },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    }
-  }
-});
+    breakpoints: {
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      500: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
+
+  const sliderElement = document.querySelector(".work__slider");
+
+  sliderElement.addEventListener("mouseenter", () => {
+    swiper.autoplay.stop();
+  });
+
+  sliderElement.addEventListener("mouseleave", () => {
+    swiper.autoplay.start();
+  });
+}
+
+function usersSlider(newDelay) {
+  const swiper = new Swiper(".user__sc", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    loop: false,
+    autoplay: {
+      delay: newDelay,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".users__info_btn-right",
+      prevEl: ".users__info_btn-left",
+    },
+  });
+
+  const sliderElement = document.querySelector(".user__sc");
+
+  sliderElement.addEventListener("mouseenter", () => {
+    swiper.autoplay.stop();
+  });
+
+  sliderElement.addEventListener("mouseleave", () => {
+    swiper.autoplay.start();
+  });
+}
 
 
-const sliderElement = document.querySelector('.work__slider');
 
-sliderElement.addEventListener('mouseenter', () => {
-  swiper.autoplay.stop();
-});
-
-sliderElement.addEventListener('mouseleave', () => {
-  swiper.autoplay.start();
-});
+workSlider(2500);
+usersSlider(1500)
